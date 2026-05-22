e6d494f34fe8ce1beb4c68a18ecfade2bd8a3a12
willowwanglala-glitch <willowwanglala@gmail.com>
2026-05-22 22:01:54 +0800

    Delete miniprogram-1 directory

--- miniprogram-1/miniprogram-1/cloudfunctions/cozeChat/config.js
+++
@@ -1,5 +0,0 @@
-// 从 miniprogram/utils/coze.js 迁移；建议在 Coze 控制台重新生成 Token 后更新此处
-module.exports = {
-  token: 'Bearer pat_sIyTpJPOX7k8Ii7RVaPbktWv3BkjgvNOlQRl4YDnMq0gleou9dHuhIczOLzAfu6A',
-  botId: '7642226137674219526'
-};
--- miniprogram-1/miniprogram-1/miniprogram/utils/coze.config.local.js
+++
@@ -1,6 +0,0 @@
-// 本地 Coze 配置（已在 .gitignore 中忽略，勿提交公开仓库）
-module.exports = {
-  baseURL: 'https://api.coze.cn/open_api/v2/chat',
-  token: 'Bearer pat_sIyTpJPOX7k8Ii7RVaPbktWv3BkjgvNOlQRl4YDnMq0gleou9dHuhIczOLzAfu6A',
-  botId: '7642226137674219526'
-};
